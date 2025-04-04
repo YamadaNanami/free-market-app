408c8e6ff6d5ca5217166a3fee27743b5742ac37
Haba Nanami <nananab521@gmail.com>
2025-04-04 10:44:46 +0900

    テーブル作成

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('name',255);
+            $table->string('img_url',8190);
+            $table->string('email',255)->unique();
+            $table->string('password',255);
+            $table->text('post');
+            $table->string('address',255);
+            $table->string('building',255);
             $table->rememberToken();
             $table->timestamps();
         });
